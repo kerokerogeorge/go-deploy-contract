3e0378ba082d1220f704313dd02d3a695060be18
kerokerogeorge <kerokerogeorge@yahoo.co.jp>
2022-10-28 16:25:04 +0900

    load smart contract

---
+++ handler/load_smart_contract.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+	storage "github.com/kerokerogeorge/go-deploy-smartcontract/contracts"
+)
+
+func LoadSmartContract() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err := ethclient.Dial(os.Getenv("URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	address := common.HexToAddress("0x06f447ce56C6cE7C40F20EFf118dB6Bb4fa60148")
+	instance, err := storage.NewStorage(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("contract is loaded")
+	_ = instance
+}
